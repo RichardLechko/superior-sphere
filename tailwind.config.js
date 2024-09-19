ed2029e8e455cf442a26cc0bed811819bd3ef21c
RichardLechko <richardlechko04@gmail.com>
2024-09-19 18:06:27 -0500

    initalized a astro project

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
